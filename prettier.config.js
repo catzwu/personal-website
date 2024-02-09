c786245a943fc51f004fed9186d0aed1ec19035f
Catherine Wu <catw2188@gmail.com>
2024-02-09 12:02:27 -0500

    Add tailwind prettier plugin

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    plugins: ['prettier-plugin-tailwindcss'],
+    tailwindFunctions: ['clsx', 'tw'],
+  };
+  
+  export default config;
+
