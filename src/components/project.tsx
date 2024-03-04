abd5521857c884aa0e4e71c386c97fee9867527b
Catherine Wu <catw2188@gmail.com>
2024-03-04 21:34:33 +0800

    Update project styling

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -9,16 +9,18 @@ type ProjectProps = {
 
 const Project: FC<ProjectProps> = ({ name, roles, image, description }) => {
     return (
-        <div className="group relative max-w-md overflow-clip rounded-md border border-secondary-200 p-4">
-            {/* note that this is going to be hard-coded to two lines of description, jk that doesn't work for screen sizes */}
-            <h3 className="mb-2 text-2xl font-light text-secondary-200 lg:text-3xl">
-                {name}
-            </h3>
-            <h4 className="mb-4 text-lg font-extralight italic lg:text-xl">
-                {roles}
-            </h4>
-            <p className="mb-2 ">{description}</p>
-            <img src={image} />
+        <div className="relative flex max-w-lg flex-row items-start justify-between space-x-4 rounded-md border border-secondary-200 p-4">
+            <div>
+                <h3 className="mb-2 text-2xl font-light text-secondary-200 lg:text-3xl">
+                    {name}
+                </h3>
+                <h4 className="mb-4 text-lg font-extralight italic lg:text-xl">
+                    {roles}
+                </h4>
+                <p className="mb-2 ">{description}</p>
+            </div>
+
+            <img src={image} className="h-32" />
         </div>
     )
 }
