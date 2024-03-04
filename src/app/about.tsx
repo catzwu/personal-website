d4520b20a05f6b9f7c0f8081df2735699ba25ac2
Catherine Wu <catw2188@gmail.com>
2024-03-04 21:57:05 +0800

    Add contact info tooltip

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link'
+import { useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { Tooltip } from 'react-tooltip'
 
 const email = 'catw2188@gmail.com'
 
 const About = () => {
+    const [tooltip, setTooltip] = useState<string>('Click to copy email')
+
     return (
         <div className="animate-mount-component space-y-4">
             <h1 className="mb-8 text-4xl font-bold leading-loose md:text-5xl lg:text-6xl">
@@ -44,11 +48,23 @@ const About = () => {
                 >
                     Resume
                 </Link>
-                <CopyToClipboard text={email}>
+                <CopyToClipboard
+                    text={email}
+                    onCopy={() => setTooltip('Copied!')}
+                >
                     <span className="cursor-pointer" id="contact">
                         Contact
                     </span>
                 </CopyToClipboard>
+                <Tooltip
+                    anchorSelect="#contact"
+                    place="bottom"
+                    content={tooltip}
+                    className="bg-white"
+                    noArrow={true}
+                    afterHide={() => setTooltip('Click to copy email')}
+                    style={{ backgroundColor: '#02190C' }}
+                />
 
                 <Link href="https://www.admonymous.co/catzwu">Feedback</Link>
                 <Link href="/brain">???</Link>
