dd9fc1bcbc561aa53a069c092c4312ea4d7938a8
Catherine Wu <catw2188@gmail.com>
2024-05-06 15:31:12 -0500

    Update image order and content

--- src/components/learning.tsx
+++ src/components/learning.tsx
@@ -16,18 +16,18 @@ const Learning: FC<PropsWithChildren<LearningProps>> = ({
     return (
         <div
             className="cursor-pointer flex-col rounded-md border border-secondary-200 p-4 transition-all duration-300 hover:bg-secondary-50"
-            onClick={onClick}
+            onClick={onClick} // TODO: add smarter behavior so you can e.g. highlight stuff inside
         >
             <h3 className="mb-2 text-xl font-light text-secondary-200 lg:text-2xl">
                 {name}
             </h3>
             <div
                 className={clsx(
-                    { 'max-h-0': !isOpen, 'max-h-[40rem]': isOpen },
+                    { 'max-h-0': !isOpen, 'max-h-[40rem]': isOpen }, // TODO: scroll slightly so the full open thing is centered
                     'flex overflow-auto transition-all duration-300'
                 )}
             >
-                <div className="my-auto font-light">{children}</div>
+                <div className="my-auto p-1 font-light">{children}</div>
             </div>
         </div>
     )
